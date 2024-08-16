cb0e84c5602b3b2cac04c0154df7e9312fc676c3
LikeAWalnut <118017220+LikeAWalnut@users.noreply.github.com>
2024-08-16 12:24:25 +0200

    Update minecraft-installer.ps1

--- Minecraft-Installer/minecraft-installer.ps1
+++ Minecraft-Installer/minecraft-installer.ps1
@@ -1,7 +1,7 @@
-cd C:\
-mkdir MinecraftLauncher
-cd MinecraftLauncher
-Invoke-WebRequest -Uri "https://launcher.mojang.com/download/Minecraft.exe" -OutFile "./MinecraftInstaller.exe"
+cd C:\;
+mkdir C:\MinecraftLauncher;
+cd C:\MinecraftLauncher;
+Invoke-WebRequest -Uri "https://launcher.mojang.com/download/Minecraft.exe" -OutFile "./MinecraftInstaller.exe";
 Start-Process -FilePath "./MinecraftInstaller.exe" -ArgumentList '/install /quiet /norestart' -Wait
 Remove-Item -Path "./MinecraftInstaller.exe"
 start .\MinecraftLauncher.exe
